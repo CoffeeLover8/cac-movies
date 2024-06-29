84d6aa938ae24f7a444976db57a05b2a8c92b190
CoffeeLover8 <mariadelosangeles.rol8@gmail.com>
2024-06-29 16:49:14 -0300

    Estilos de la página de registro terminada

--- catApi.js
+++ catApi.js
@@ -5,15 +5,20 @@ const main = document.getElementById("mainApi");
 const endPoint = "https://api.thecatapi.com/v1/images/search?limit=10";
 
 const obtenerGatitos = async () => {
-    const res = await fetch(endPoint);
-    const data = await res.json();
-    console.log(data);
+    try {
+        const res = await fetch(endPoint);
+        const data = await res.json();
+        console.log(data);
 
-    data.forEach( unGatito => {
-        main.innerHTML += `
-            <img src=${unGatito.url} width="200px">
-        `
-    });
+        data.forEach(unGatito => {
+            main.innerHTML += `
+                <img src=${unGatito.url} width="200px">
+            `
+        });
+    } catch {
+        alert("Lo sentimos, ocurrió un error");
+    }
+    
 }
 
 obtenerGatitos();
